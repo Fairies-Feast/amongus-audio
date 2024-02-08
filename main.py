d6dc3460867e88c41f7f9ae28bff6018cc11d2a5
fairies0feast <86023563+Fairies-Feast@users.noreply.github.com>
2024-02-07 20:26:56 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+app = Flask('app')
+playAudio = False
+
+
+@app.route('/')
+def hello_world():
+  return render_template('play.html')
+
+
+@app.route('/status')
+def getStatus():
+  return str(playAudio)
+@app.route("/toggle")
+def toggleAudio():
+  global playAudio
+  playAudio = not playAudio
+  return "OK"
+
+app.run(host='0.0.0.0', port=8080)
